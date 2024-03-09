8d400d297f5821dfc263b115f50551a254e31c1f
Mehmet Salih Bozkır <150898451+MehmetBozkir@users.noreply.github.com>
2024-03-09 19:47:18 +0300

    add loading efect

---
+++ app/loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function loading() {
+  return (
+    <div className="grid place-items-center">
+      <span className="loading loading-spinner text-primary text-center w-3/12"></span>
+    </div>
+  );
+}
+
+export default loading;
